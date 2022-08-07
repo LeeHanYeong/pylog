10837bf903021703069ea05b9a0588d83519c474
lhy <dev@lhy.kr>
2022-08-07 19:45:38 +0900

    [pylog] 14.3. View에서 POST요청 처리
    - request.method가 POST일때 글 생성 후 상세페이지로 이동

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from blog.models import Post
 
@@ -20,4 +20,12 @@ def post_detail(request, post_id):
 
 
 def post_add(request):
+    if request.method == "POST":
+        title = request.POST["title"]
+        content = request.POST["content"]
+        post = Post.objects.create(
+            title=title,
+            content=content,
+        )
+        return redirect(f"/posts/{post.id}/")
     return render(request, "post_add.html")
